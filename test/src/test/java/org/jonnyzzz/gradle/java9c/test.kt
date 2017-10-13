96c0715f71afed765f8054e5e48ef05f413a6007
Eugene Petrenko <eugene.petrenko@gmail.com>
2017-10-13 22:28:43 +0200

    move

--- test/src/test/java/org/jonnyzzz/gradle/java9c/test.kt
+++ test/src/test/java/org/jonnyzzz/gradle/java9c/test.kt
@@ -6,37 +6,6 @@ import org.junit.Test
 import org.junit.rules.TemporaryFolder
 import java.io.File
 
-interface FileBuilder {
-  operator fun String.unaryMinus()
-
-  fun fileWriter(name: String, builder: FileBuilder.() -> Unit)
-}
-
-interface GradleBuilder : FileBuilder
-
-private fun fileWriter(dir: File, name: String, builder: FileBuilder.() -> Unit) {
-  val text = buildString {
-
-    object : FileBuilder {
-      override fun String.unaryMinus() {
-        appendln(this)
-      }
-
-      override fun fileWriter(name: String, builder: FileBuilder.() -> Unit) {
-        fileWriter(dir, name, builder)
-      }
-
-    }.builder()
-  }
-
-  File(dir, name).apply {
-    println("File $this: ")
-    println(text)
-    println()
-    parentFile.mkdirs()
-    writeText(text)
-  }
-}
 
 class IntegrationTest {
 
