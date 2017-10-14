e76a6699bf4fc741b6a248f01119bb842732a0a8
Eugene Petrenko <eugene.petrenko@gmail.com>
2017-10-14 02:31:16 +0200

    scan for packages: a trivial implementation for Jar and FS, no reporting yet

--- plugin/src/main/java/org/jonnyzzz/gradle/java9c/java9c.kt
+++ plugin/src/main/java/org/jonnyzzz/gradle/java9c/java9c.kt
@@ -7,6 +7,10 @@ import org.gradle.api.file.FileCollection
 import org.gradle.api.plugins.JavaBasePlugin
 import org.gradle.api.plugins.JavaPluginConvention
 import org.gradle.api.tasks.TaskAction
+import java.io.File
+import java.nio.file.Files
+import java.util.jar.JarFile
+import kotlin.streams.asSequence
 
 
 open class GradlePlugin : Plugin<Project> {
@@ -21,7 +25,7 @@ open class GradlePlugin : Plugin<Project> {
       val scanTask = project.tasks.maybeCreate("java9c_${set.name}", ScanClasspathTask::class.java)
 
       scanTask.classpath = { set.runtimeClasspath }
-//      scanTask.dependsOn(project.configurations.getByName(set.runtimeClasspathConfigurationName))
+      scanTask.dependsOn(project.configurations.getByName(set.runtimeClasspathConfigurationName))
 
       task.dependsOn(scanTask)
     }
@@ -35,7 +39,38 @@ open class ScanClasspathTask : DefaultTask() {
   fun `execute java9c task`() {
     println("project: " + project.name)
 
-    println("classpath: " + classpath()?.toSet())
+    val fileSet : FileCollection = classpath() ?: return
+    println("classpath: " + fileSet.toSet())
 
+    fileSet.toSet().map {
+      it to (listAppPackagesFromFile(it) + listAppPackagesFromJar(it))
+    }.map {
+      println("Tree: ${it.first} -> ${it.second}")
+    }
+  }
+}
+
+fun listAppPackagesFromFile(root: File): Set<String> {
+  if (!root.isDirectory) return emptySet()
+
+  val rootPath = root.toPath()
+  return Files.walk(rootPath)
+          .asSequence()
+          .filter { it.fileName.endsWith(".class") }
+          .map { rootPath.relativize(it.parent).toFile().path }
+          .map { it.replace('/', '.') }
+          .toSortedSet()
+}
+
+fun listAppPackagesFromJar(root: File): Set<String> {
+  if (!root.isFile) return emptySet()
+
+  return JarFile(root).use { jar ->
+    jar.entries().asSequence()
+            .filter { !it.isDirectory }
+            .filter { it.name.endsWith(".class") }
+            .map { it.name.substring(0, it.name.lastIndexOf('/')) }
+            .map { it.replace('/', '.') }
+            .toSortedSet()
   }
 }
