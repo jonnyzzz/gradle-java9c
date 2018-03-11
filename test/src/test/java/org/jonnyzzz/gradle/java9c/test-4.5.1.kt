cb5e1755363cb291beeca71b05c9b883be17ac91
Eugene Petrenko <eugene.petrenko@gmail.com>
2018-03-11 23:04:30 +0100

    add tests for Gradle 4.5.1 and 4.6

---
+++ test/src/test/java/org/jonnyzzz/gradle/java9c/test-4.5.1.kt
@@ -0,0 +1,5 @@
+package org.jonnyzzz.gradle.java9c
+
+open class IntegrationTest_4_5_1 : IntegrationTest_4_4_1() {
+  override val gradleVersion = "4.5.1"
+}
---
+++ test/src/test/java/org/jonnyzzz/gradle/java9c/test-4.6.kt
@@ -0,0 +1,5 @@
+package org.jonnyzzz.gradle.java9c
+
+open class IntegrationTest_4_6 : IntegrationTest_4_4_1() {
+  override val gradleVersion = "4.6"
+}
